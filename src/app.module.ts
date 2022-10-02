073f81da4e4120652ee5a11b63f75e3c891856eb
Francisco Duarte <francisco.caramez.duarte@gmail.com>
2022-10-02 22:21:08 +0100

    adds basic db config for postgres and installs typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
