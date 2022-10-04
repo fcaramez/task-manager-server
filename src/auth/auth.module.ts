dfc7e4728459e816ef761cdac95f18d03e2dd0b9
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-04 18:05:40 +0100

    adds signup feature with bcrypt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
