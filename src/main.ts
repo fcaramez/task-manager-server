51128b767e85eedf30d69c2c7346677cd850c704
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-08 10:40:10 +0100

    adds config with joi

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalInterceptors(new TransformInterceptor());
